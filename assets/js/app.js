db17d00079196283214950d9c08ba735215651dc
akjmalamin7 <thpi205526@gmail.com>
2021-11-04 07:35:02 +0600

    banner section

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+(function(){
+    const heaerMiddleNav = document.getElementById('heaerMiddleNav')
+    const toggleIconWrapper = document.getElementById('toggleIconWrapper')
+
+    toggleIconWrapper.addEventListener('click', function(e){
+        heaerMiddleNav.classList.toggle("showMobileMenu")
+    })
+
+    // right nav
+    const clickHeaderRightNav = document.getElementById('clickHeaderRightNav')
+    const showHeaderRightNav = document.getElementById('showHeaderRightNav')
+
+    clickHeaderRightNav.addEventListener('click', function(e){
+        showHeaderRightNav.classList.toggle("showRightNav")
+        console.log("yes")
+    })
+})()
+
+ 
